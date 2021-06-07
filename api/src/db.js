8bd501d17b231f87ebafca7371b69e0a5aa3a270
Santiago <santiagorosales1@outlook.com>
2021-06-06 22:41:05 -0300

    dDSAa

--- api/src/db.js
+++ api/src/db.js
@@ -51,7 +51,6 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Videogame, Genre } = sequelize.models;
 
