17d1a9b3e250c416ce26f871b34511083e2104bb
Santiago <santiagorosales1@outlook.com>
2021-06-06 23:53:56 -0300

    sañjdsdas

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import dotenv from "dotenv"
 import axios from 'axios';
+
 dotenv.config()
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
-fetch.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
-
+/* fetch.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+ */
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
