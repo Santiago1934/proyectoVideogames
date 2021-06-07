6f82798a56101faa98f132055582be130fd876bc
Santiago <santiagorosales1@outlook.com>
2021-06-06 21:36:06 -0300

    proband

--- api/src/db.js
+++ api/src/db.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-let sequelize =
+export let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
         port: 5432,
-        username: DB_USER,
+        username: "queloke",
         password: DB_PASSWORD,
         pool: {
           max: 3,
