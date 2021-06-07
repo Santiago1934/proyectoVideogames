70a20c9cabe3c31c78fd2094b0b5bd484fc1003b
Santiago <santiagorosales1@outlook.com>
2021-06-06 23:11:01 -0300

    dDSAaaAaaasddas

--- api/src/routes/home.js
+++ api/src/routes/home.js
@@ -4,12 +4,12 @@ const {API_KEY} = process.env;
 
 const Home = (req, res) => {
     if(req.query.search){
-    axios.get(`http://api.rawg.io/api/games?search=${req.query.search}&key=${API_KEY}`)
+    axios.get(`https://api.rawg.io/api/games?search=${req.query.search}&key=${API_KEY}`)
     .then( data => data)
     .then(response => res.send(response.data))
     }
     else {
-    axios.get(`http://api.rawg.io/api/games?key=${API_KEY}`)
+    axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
     .then( data => data)
     .then(response => res.send(response.data))
     }
